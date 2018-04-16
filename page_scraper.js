e4f5a833c82185cc3be705b0f56faa1b8cff84ad
Emma Ling <emmaling@Emmas-MacBook-Pro-3.local>
2018-04-15 22:10:31 -0400

    messages work

--- page_scraper.js
+++ page_scraper.js
@@ -3,17 +3,26 @@ webpage and send it to the background for analysis. */
 
 'use strict';
 
-// Listen for messages
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-    // If the received message has the expected format...
-    if (msg.text === 'report_back') {
-        // Call the specified callback, passing
-        // the web-page's DOM content as argument
-        sendResponse(document.all[0].outerHTML);
-    }
-});
+// // Listen for messages
+// chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+//     // If the received message has the expected format...
+//     if (msg.text === 'report_back') {
+//         // Call the specified callback, passing
+//         // the web-page's DOM content as argument
+//         sendResponse(document.all[0].outerHTML);
+//     }
+// });
 
 
-chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
-  console.log(response.farewell);
-});
+// chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+//   console.log(response.farewell);
+// });
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if (request.greeting == "hello")
+      sendResponse({farewell: "goodbye"});
+  });
--- popup.js
+++ popup.js
@@ -25,7 +25,11 @@ changeColor.onclick = function(element) {
     chrome.tabs.executeScript(
         {code: 'document.body.style.backgroundColor = "' + color + '";'});
 
-        
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+          console.log(response.farewell);
+        });
+      });
         // chrome.tabs.getCurrent(function(tab) {
         //   tabID = tab.id;
         //   chrome.tabs.sendMessage(tabID, {text: 'report_back'}, get_news);
