26fb61477bccb7c440963a10b0076f4fe9320af8
Emma Ling <emmaling@Emmas-MacBook-Pro-3.local>
2018-04-23 11:51:59 -0400

    added some comments

--- popup.js
+++ popup.js
@@ -30,7 +30,9 @@ changeColor.onclick = function(element) {
           console.log(response.response);
         });
       });
+      // Put placeholder into the popup html
       document.getElementById("demo").innerHTML = "something";
+      // Send data to Fakebox
       let xhttp = new XMLHttpRequest();
       xhttp.open("POST", "http://localhost:8080/fakebox/check", true);
       xhttp.setRequestHeader("Accept", "application/json; charset=utf-8")
@@ -40,6 +42,7 @@ changeColor.onclick = function(element) {
         "title": "Chemical Weapons Experts Blocked From Site of Syria Attack, Officials Say",
         "content": 'Senior Russian diplomats said it was the United Nations, not Syria or Russia, that had prevented inspectors from entering Douma. “The problem was the absence of the U.N. Secretariat security department’s approval for O.P.C.W. experts to visit Douma,” Sergei A. Ryabkov, deputy foreign minister of Russia, told reporters, according to the news agency Interfax. A spokesman for the United Nations, Stéphane Dujarric, disputed the Russian explanation. “The U.N. has given them all the necessary clearances,” he said by telephone. “We’re supporting the team as much as we can.” Later at his regular noon briefing, Mr. Dujarric said of the O.P.C.W. experts: “We’ve not denied them any sort of clearance.” Mr. Dujarric declined to say whether Secretary General António Guterres would demand that Russia and Syria provide the experts with access to the Douma site. But the spokesman said Mr. Guterres wanted the investigation to move forward “so we can have a full picture of all the facts.”'
       }));
+      // When Fakebox responds, put Fakebox data in the popup
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
           document.getElementById("demo").innerHTML = this.responseText;
