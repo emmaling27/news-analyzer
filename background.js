3d2914b4673fa0e49d67ed7cc2494ca1eadde494
Emma Ling <emmaling@Emmas-MacBook-Pro-3.local>
2018-04-22 20:56:15 -0400

    changed page action to browser action, but now it only works on some

--- background.js
+++ background.js
@@ -11,15 +11,15 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({color: '#3aa757'}, function() {
     console.log("The color is green.");
   });
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'},
-      })
-      ],
-          actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
+  // chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+  //   // chrome.declarativeContent.onPageChanged.addRules([{
+  //   //   conditions: [new chrome.declarativeContent.PageStateMatcher({
+  //   //     pageUrl: {hostEquals: 'developer.chrome.com'},
+  //   //   })
+  //   //   ],
+  //   //       actions: [new chrome.declarativeContent.ShowPageAction()]
+  //   // }]);
+  // });
 });
 
 console.log("hi");
